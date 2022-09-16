429f9a3e11327104d84700d6316c26c83b65ea78
Fabio Diogo <fabindiogo@gmail.com>
2022-09-15 21:58:11 -0300

    Criado model de user

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -13,9 +13,14 @@ const postSchema= Joi.object({
 })
 
 const signup = createHandler()
-  .post(validate({ body: postSchema}), (req,res) => {
-    signupUser(req.body)
-    res.status(200).json({ teste: "ok"})
+signup.post(validate({ body: postSchema}), async (req,res) => {
+    try{
+      const user = await signupUser(req.body)
+      res.status(201).json({user})
+    } catch (err) {
+      console.error(err)
+      throw err
+    }
   })
 
 export default signup
