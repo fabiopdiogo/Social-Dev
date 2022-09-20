6a1c1345a4c479e650045a5a2dcfe387541e557b
Fabio Diogo <fabindiogo@gmail.com>
2022-09-19 21:40:04 -0300

    criado sistema de login

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -6,11 +6,8 @@ import validate from '../../../lib/middlewares/validation'
 import { login } from '../../../modules/user/user.service'
 
 import { ironConfig } from '../../../lib/middlewares/ironSession'
+import { loginSchema } from '../../../modules/user/user.schema'
 
-const loginSchema = Joi.object({
-  userOrEmail: Joi.string().required(),
-  password: Joi.string().required()
-})
 
 const handler = createHandler()
 
@@ -22,10 +19,9 @@ handler.post(validate({ body: loginSchema }), async (req,res) =>{
       user: user.user
     }
     await req.session.save()
-    res.send({ ok: true})
+    res.send({ ok: true })
   } catch (err){
-   console.error(err)
-   throw err
+   return res.status(400).send(err.message)
   }
 })
 
